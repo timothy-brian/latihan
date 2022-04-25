6d9e50c79d81bfcfc7622455ed5c37239068709a
Timothy Brian <73934414+timothy-brian@users.noreply.github.com>
2022-04-25 15:30:28 +0700

    phpdasar
    menambahkan hasil belajar php dasar

---
+++ phpdasar-20220425T082617Z-001/phpdasar/pertemuan2/index.php
@@ -0,0 +1,64 @@
+<?php 
+// pertemuan 2 - php dasar
+// sintaks php
+
+//  standar output
+//  echo, print
+//  print_r
+// var_dump
+
+// penulisan sintaks php
+// 1. php di dalam html
+// 2. html di dalam php
+
+// variabel dan tipe data
+// variabel
+// tidak boleh diawali dengan angka, tapi boleh mengandung angka
+
+// $nama = "timothy brian";
+
+// echo "halo, nama saya $nama";
+
+// operator
+// aritmatika
+// + - * / %
+
+// $x = 10;
+// $y = 20;
+
+// echo $x * $y;
+
+// penggabung sring / concatenation / concat
+// .
+
+// $nama_depan = "timothy";
+// $nama_belakang = "brian";
+// echo $nama_depan ." ". $nama_belakang;
+
+// assignment
+// =, +=, -=, *=, /=, %=, .=
+
+// $x = 1;
+// $x -= 5;
+// echo $x;
+
+// $nama = "timothy";
+// $nama .= " ";
+// $nama .= "brian";
+// echo $nama;
+
+// perbandingan
+// <, >, <=, >=, ==, !=
+
+// var_dump(1 == "1");
+
+// identitas
+// ===, !==
+
+// var_dump(1 === "1");
+
+// logika
+// &&, ||, !
+
+// $x = 30;
+// var_dump($x < 20 || $x % 2 == 0);
